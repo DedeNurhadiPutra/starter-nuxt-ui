d1043a9c4f630b831d65d0cef80d500e683621de
Dede Nurhadi Putra <dede@recis.co.id>
2025-05-02 16:56:25 +0700

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/image',
+    '@nuxt/test-utils',
+    '@nuxt/ui',
+    '@nuxtjs/seo',
+    '@pinia/nuxt',
+    'nuxt-og-image',
+    '@nuxtjs/critters',
+    '@vueuse/nuxt'
+  ],
+  css: ['~/assets/css/main.css']
+});
---
+++ site.ts
@@ -0,0 +1,32 @@
+// since `.js, .ts` files are not included by default,
+// the following comment tells UnoCSS to force scan this file (to pick the logo icon).
+// @unocss-include
+
+export default {
+  title: 'NuCommerce',
+  description: 'Vue Designer Nuxt Tailwind CSS / Nuxt UI - Quick start template',
+  logo: 'i-fluent-emoji:shopping-bags',
+  author: 'Pinegrow',
+  url: 'https://pg-nuxtui.netlify.app',
+  github: 'https://github.com/pinegrow/pg-nuxtui',
+  ogImageUrl: 'og-image.jpg', // absolute url (or) from public folder
+  generator: 'https://vuedesigner.com',
+  defaultLocale: 'en', // default
+  identity: {
+    type: 'Organization'
+  } as const,
+  twitter: '@vuedesigner',
+  trailingSlash: false, // default
+  titleSeparator: '|', // default
+
+  navs: {
+    primary: [{ title: 'Home', to: '/', icon: 'i-mdi-home' }],
+    secondary: [
+      // {
+      //   title: 'Subscribe',
+      //   icon: 'i-mdi-home',
+      //   to: '/subscribe',
+      // },
+    ]
+  }
+};
